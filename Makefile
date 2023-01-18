3981784aa4a7a36b841d632c35484e697282579e
tzeck1 <icekex23@gmail.com>
2023-01-18 14:15:03 +0100

    prob done

--- Makefile
+++ Makefile
@@ -1,5 +1,4 @@
 NAME=inception
-CONTS:=$(shell sudo docker ps -q | wc -l)
 
 ${NAME}:
 	sudo docker-compose -f srcs/docker-compose.yml up --build
@@ -7,6 +6,8 @@ ${NAME}:
 clean:
 	# sudo docker kill $(shell sudo docker ps -q)
 	sudo docker system prune -a
+
+reset:
 	sudo rm -rf ~/data/wordpress-data/*
 	sudo rm -rf ~/data/mariadb-data/*
 
--- srcs/requirements/wordpress/entry.sh
+++ srcs/requirements/wordpress/entry.sh
@@ -3,6 +3,15 @@
 wp core install --url=${DOMAIN} --title=${TITLE} --admin_user=${WP_ROOT_USER} --admin_password=${WP_ROOT_PWD} \
                 --admin_email=${WP_ROOT_MAIL} --allow-root
 
-wp user create ${WP_USER} ${WP_MAIL} --role=administrator --user_pass=${WP_PWD} --allow-root
+export user_check=$(wp user list | grep "test")
+export admin_check=$(wp user list | grep "tzeck")
+
+if [ -z "$user_check" ]; then
+    wp user create --quiet ${WP_USER} ${WP_MAIL} --user_pass=${WP_PWD}
+fi
+
+if [ -z "$admin_check" ]; then
+    wp user create --quiet ${WP_ROOT_USER} ${WP_ROOT_MAIL} --role=administrator --user_pass=${WP_ROOT_PWD} --allow-root
+fi
 
 php-fpm8 -F
